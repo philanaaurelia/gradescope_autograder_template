2da72cb104d926927319abf9eaa14d3a805a1700
Phil B. <philanaaurelia@gmail.com>
2025-08-10 16:07:34 -0400

    Delete apt-get.sh

--- apt-get.sh
+++
@@ -1,9 +0,0 @@
-#apt-get install -y python python-pip python-dev
-
-apt-get install -y g++
-apt-get install -y jsoncpp
-
-apt-get install -y libjsoncpp-dev
-
-apt-get install -y clang-3.9  
-ln -s /usr/bin/clang++-3.9 /usr/bin/clang++

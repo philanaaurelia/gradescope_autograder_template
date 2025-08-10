2ecc76c23122f1d6481d759f66e3646af588c7a5
Phil B. <philanaaurelia@gmail.com>
2025-08-10 14:51:54 -0400

    Update divider.h

--- REFERENCE-SOLUTION/divider.h
+++ REFERENCE-SOLUTION/divider.h
@@ -1,1 +1,15 @@
+#ifndef DIVIDER_H
+#define DIVIDER_H
 
+template <typename T>
+class Divider {
+  public:
+  T GetQuotient(T, T);
+};
+
+template <typename T>
+T Divider<T>::GetQuotient(T num, T den) {
+  return (num / den);
+}
+
+#endif
